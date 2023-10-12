bd3ee9d4fd59b78f93bea202fc3fa3a790a8912b
Cormac Larkin <cormac.larkin@c02g70dqml85.client.kainos.com>
2023-10-12 12:29:43 +0100

    Added test data and updated foreign key creation

--- feature_01.sql
+++ feature_01.sql
@@ -1,10 +1,14 @@
 DELIMITER $$
 
-    -- Create the database
-    CREATE DATABASE kainoos_softwoorn_CormacL;
+    -- Use the database
+    USE kainoos_softwoorn_CormacL $$
+
+DROP PROCEDURE IF EXISTS feature_1 $$
+CREATE PROCEDURE feature_1()
+DELIMITER $$
 
     -- Use the database
-    USE kainoos_softwoorn_CormacL;
+    USE kainoos_softwoorn_CormacL $$
 
 DROP PROCEDURE IF EXISTS feature_1 $$
 CREATE PROCEDURE feature_1()
@@ -25,14 +29,20 @@ BEGIN
         national_insurance_number CHAR(9),
         bank_account_number CHAR(8),
         employee_type_id SMALLINT,
-        commission_rate DECIMAL(2,2)
+        commission_rate DECIMAL(10,2),
+        FOREIGN KEY (employee_type_id) REFERENCES employee_type(employee_type_id)
     );
-    
-    -- Add foreign key constraint
-    ALTER TABLE employee 
-    ADD CONSTRAINT fk_employee_employee_type
-    FOREIGN KEY (employee_type_id)
-    REFERENCES employee_type(employee_type_id);
+
+    -- Add sample/test data for employee_type
+    INSERT INTO employee_type (employee_type_name) VALUES ('Sales'), ('Delivery'), ('HR');
+
+    -- Add sample employees 
+    INSERT INTO employee (`name`, salary, national_insurance_number, bank_account_number, employee_type_id, commission_rate)
+    VALUES 
+    ('Sample Employee_1', 50000, '123456789', '12345678', 1, 5.5),
+    ('Sample_Employee_2', 35000, '987654321', '23456789', 2, NULL),
+    ('Sample_Employee_3', 54000, '345678987', '34455555', 3, NULL);
+
     
 END $$
 DELIMITER ;
