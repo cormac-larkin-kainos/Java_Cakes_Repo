e878d1ea6ab36452b4a543fa8431c6c2014e3dd9
Ava Deery <ava.deery@gmail.com>
2023-10-12 14:41:08 +0100

    Feature edited as per comments to add primary key and select statement

--- feature_04.sql
+++ feature_04.sql
@@ -8,6 +8,7 @@ BEGIN
 	START TRANSACTION;
 
     CREATE TABLE project_employee(
+        project_employee_id SMALLINT PRIMARY KEY AUTO_INCREMENT,
         employee_id SMALLINT,
         project_id SMALLINT,
         currently_active BOOLEAN,
@@ -21,7 +22,17 @@ BEGIN
     (1, 1, 1, '2023-10-01', NULL),
     (2, 1, 1, '2023-10-01', NULL),
     (3, 1, 0, '2023-10-01', '2023-10-10');
-    
+
+    -- select statement to show which employees are involved with which project
+    SELECT employee.name AS Employee_Name, GROUP_CONCAT(project.name SEPARATOR ', ') AS Project_Name
+    FROM employee 
+    INNER JOIN
+    project_employee
+    ON employee.employee_id = project_employee.employee_id
+    INNER JOIN
+    project
+    ON project.project_id = project_employee.project_id
+    GROUP BY Employee_Name;
 
 	COMMIT;
 END $$
